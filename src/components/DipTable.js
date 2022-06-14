02f44e439963122a327fa0b373d0bcad8540ab31
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-06-14 16:49:42 +0300

    data edit

--- src/components/DipTable.js
+++ src/components/DipTable.js
@@ -2,22 +2,43 @@ import React from "react";
 import Table from "react-bootstrap/Table";
 
 function DipTable(props) {
-  const uasArr = props.tableContent.filter((row) => row.Dip > 0);
-  const headerKeysArr = uasArr.length == 0 ? [] : Object.keys(uasArr[0]);
-  console.log(headerKeysArr);
+  // converter function
+  const data = props.tableContent;
+  const dataShallow = [...data];
 
-  // header map
+  const keysToLowerCase = (obj) => {
+    var myKeys = Object.keys(obj);
+    var n = myKeys.length;
+    while (n--) {
+      var key = myKeys[n]; // "cache" it, for less lookups to the array
+      if (key !== key.toLowerCase()) {
+        // might already be in its lower case version
+        obj[key.toLowerCase().replace(/\s+/g, "")] = obj[key]; // swap the value to a new lower case key
+        delete obj[key]; // delete the old key
+      }
+    }
+    return obj;
+  };
+
+  const newData = dataShallow.map((row) => {
+    return keysToLowerCase(row);
+  });
+  console.log(newData);
+
+  const headerKeysArr = data.length == 0 ? [] : Object.keys(data[0]);
+
+  // Table Headers
   const theaders = headerKeysArr.map((headerName, index) => (
     <th key={index}>{headerName}</th>
   ));
 
-  // data map
-  const tData = uasArr.map((eachRow, index) => (
+  // Table Data
+  const tData = data.map((eachRow, index) => (
     <tr key={index}>
       <td>{index}</td>
-      <td>{eachRow.Name}</td>
-      <td>{eachRow.Hour}</td>
-      <td>{eachRow.Dip}</td>
+      <td>{eachRow.name}</td>
+      <td>{eachRow.hour}</td>
+      <td>{eachRow.dip}</td>
     </tr>
   ));
 
