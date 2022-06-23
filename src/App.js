8f826c892a2d04449f79030558de981e05a9fed1
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-06-23 17:06:13 +0300

    first edit for the layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import Navbar from "./components/Navbar";
-import Complaints from "./pages/Complaints";
-import Dip from "./pages/Dip";
-import Tch from "./pages/Tch";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Dip />} />
-          <Route path="/tch" element={<Tch />} />
-          <Route path="/complaints" element={<Complaints />} />
-        </Routes>
-      </Router>
+      <div class="wrapper">
+        <Sidebar />
+      </div>
     </>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { sidebarData } from "./SidebarData";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { IconContext } from "react-icons";
+import "./Sidebar.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Complaints from "../pages/Complaints";
+import Dip from "../pages/Dip";
+import Tch from "../pages/Tch";
+
+function Sidebar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  // to Toggle sidebar
+  const sidebarToggle = () => setSidebar(!sidebar);
+
+  //
+  const listComponents = sidebarData.map((item, index) => {
+    return (
+      <li key={index} className={item.cName}>
+        <Link to={item.path} className="menu-bars">
+          {item.icon}
+          <span>{item.title}</span>
+        </Link>
+      </li>
+    );
+  });
+  return (
+    <>
+      <Router>
+        <IconContext.Provider value={{ color: "#ffff" }}>
+          <div className="navbar">
+            <Link to="#" className="menu-bars">
+              <FaIcons.FaBars onClick={sidebarToggle} />
+            </Link>
+            <span className="navbar-text">RF Optimization Tool 📶</span>
+          </div>
+          <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items" onClick={sidebarToggle}>
+              <li className="nav-menu-toggle">
+                <Link to="#" className="menu-bars">
+                  <AiIcons.AiOutlineClose />
+                </Link>
+              </li>
+              {listComponents}
+            </ul>
+            <div className="legal">
+              &copy; Eng.Abdallah Alaff. All rights reserved.
+            </div>
+          </nav>
+        </IconContext.Provider>
+        <Routes>
+          <Route path="/" element={<Dip />} />
+          <Route path="/tch" element={<Tch />} />
+          <Route path="/complaints" element={<Complaints />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default Sidebar;
