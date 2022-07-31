a284dd3594a350efd0392456cb4ff032e313e672
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-07-31 17:39:50 +0300

    Fixing re-rendering all the component => react-window DIP report 2

--- src/components/TableComp.js
+++ src/components/TableComp.js
@@ -4,12 +4,6 @@ import { FixedSizeList as List } from "react-window";
 
 function TableComp({ ...props }) {
   const { dataArr, headerArr, refTableName, dataLength } = props;
-  let tData;
-
-  // // Table Header
-  // const tableHeader = headerArr?.map((headerName, index) => {
-  //   return <th key={index}>{headerName}</th>;
-  // });
 
   const StickyListContext = createContext();
   StickyListContext.displayName = "StickyListContext";
@@ -79,15 +73,7 @@ function TableComp({ ...props }) {
   const innerElementType = forwardRef(({ children, ...rest }, ref) => (
     <StickyListContext.Consumer>
       {({ stickyIndices }) => (
-        <Table
-          ref={ref}
-          {...rest}
-          striped
-          bordered
-          hover
-          size="sm"
-          className="my_table"
-        >
+        <Table ref={ref} {...rest} striped bordered hover size="sm">
           {stickyIndices.map((index) => (
             <StickyRow
               index={index}
@@ -112,43 +98,6 @@ function TableComp({ ...props }) {
       </List>
     </StickyListContext.Provider>
   );
-
-  // // Table Data
-  // if (refTableName === "dip") {
-  //   tData = dataArr?.map((rowObj, index) => (
-  //     <tr key={index}>
-  //       <td>{rowObj.dip}</td>
-  //       <td>{rowObj.elem}</td>
-  //       <td>{rowObj.site_name}</td>
-  //       <td>{rowObj?.date?.toDateString()}</td>
-  //       <td>{rowObj.hour}</td>
-  //       <td>{rowObj.sf}</td>
-  //       <td>{rowObj.es}</td>
-  //       <td>{rowObj.ses}</td>
-  //       <td>{rowObj.uas}</td>
-  //       <td>{rowObj.sfr}</td>
-  //       <td>{rowObj.esr}</td>
-  //       <td>{rowObj.sesr}</td>
-  //       <td>{rowObj.uasr}</td>
-  //     </tr>
-  //   ));
-  // } else if (refTableName === "tch") {
-  //   tData = ({ index, key }) => (
-  //     <tr key={key}>
-  //       <td>{dataArr[index].cell_name}</td>
-  //       <td>{dataArr[index].date}</td>
-  //       <td>{dataArr[index].hour}</td>
-  //       <td>{dataArr[index].number_of_tch_s}</td>
-  //       <td>{dataArr[index].tch_drop_rate__}</td>
-  //       <td>{dataArr[index].subscriber_percived_tch_congestion__}</td>
-  //       <td>{dataArr[index].tch_traffic_erlang}</td>
-  //       <td>{dataArr[index].sdcch_availability}</td>
-  //       <td>{dataArr[index].sdcch_drop_rate__}</td>
-  //       <td>{dataArr[index].tch_availability__}</td>
-  //       <td>{dataArr[index].cell_down_time_min}</td>
-  //     </tr>
-  //   );
-  // }
 
   console.log("TableComp renders");
   return (
